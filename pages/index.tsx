56f37e67288c53f031d3d269f83d3aae38f8a7c2
Ashfaq Mahmood <ashfaq.bd92@gmail.com>
2021-09-22 14:02:04 +0600

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import Head from "next/head";
+
+const index: FC = () => (
+  <>
+    <Head>
+      <title>DCastalia Test</title>
+    </Head>
+    <p>A simple example repo</p>
+  </>
+);
+
+export default index;
